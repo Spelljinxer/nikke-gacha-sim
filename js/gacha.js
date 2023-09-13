184dfd1180d51747b02a2e759e7d4e965b67898f
Spelljinxer <reiden556@gmail.com>
2023-09-13 18:04:58 +0800

    fix link

--- js/gacha.js
+++ js/gacha.js
@@ -1,3 +1,11 @@
+/**
+ * @author Reiden Rufin (Spelljinxer)
+ * @version 0.7
+ * @date 13/09/2023
+ * @credit I do not own the characterrs or images used in this project. All rights belong to SHIFTUP and the Level Infinite.
+ * 
+ */
+
 
 // --------------------------------------------------
 
@@ -12,37 +20,41 @@ function init(data) {
     pullCharacter('multi');
   });
 
-    //this just simply gets the query from ?activeBanner= and sets the current banner to that
-    function getQueryParameter(name) {
-      var urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get(name);
-    }
-    
-    var activeBanner = getQueryParameter("activeBanner") || "default";
-    //sets flag to true for the limitecharacter
-    console.log("activeBanner: " + activeBanner);
-    if (activeBanner != "default") {
-      var limitedCharacters = charactersData.Limited;
-      for (var i = 0; i < limitedCharacters.length; i++) {
-        var character = limitedCharacters[i];
-        if (character.name != activeBanner) {
-          character.currentBanner = false;
-        } else {
-          character.currentBanner = true;
-          break;
-        } 
-      }
-    }
-    console.log("character.currentBanner is " + character.currentBanner);
+  //this just simply gets the query from ?activeBanner= and sets the current banner to that
+  function getQueryParameter(name) {
+    var urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+  }
+  var activeBanner = getQueryParameter("activeBanner") || "default";
 
+  //sets flag to true for the limitecharacter
+  console.log("activeBanner: " + activeBanner);
+  if (activeBanner != "default") {
+    var limitedCharacters = charactersData.Limited;
+    for (var i = 0; i < limitedCharacters.length; i++) {
+      var character = limitedCharacters[i];
+      if (character.name != activeBanner) {
+        character.currentBanner = false;
+      } else {
+        character.currentBanner = true;
+        break;
+      } 
+    }
+  }
+  console.log("character.currentBanner is " + character.currentBanner);
+
+  //picks a random character from the rarity pile (fix later??)
   function getRandomCharacter(rarity) {
     var characters = charactersData[rarity];
     var randomIndex = Math.floor(Math.random() * characters.length);
     return characters[randomIndex];
   }
 
+  //does a single or multi pull
   function pullCharacter(pullType) {
     var gachaWindow = document.querySelector(".gacha-window");
+
+    //the main body of the gacha
     function doPull() {
       var rarity;
       var randomSSR = Math.random();
@@ -82,30 +94,35 @@ function init(data) {
     }
   }
 
- function displayCharacter(character, gachaWindow) {
+  //displays the character to the output (gacha-window screen)
+  function displayCharacter(character, gachaWindow) {
     try {
-        var characterImage = document.createElement("img");
-        console.log("you pulled " + character.name);
+      var characterImage = document.createElement("img");
+      console.log("you pulled " + character.name);
 
-        characterImage.src = character.image;
-        characterImage.alt = character.name;
-        characterImage.className = "gacha-card"; 
-        // Determine rarity from category
-        var category = Object.keys(charactersData).find(key => charactersData[key].includes(character));
-        if (category) {
-            if (category === 'SSR') {
-                characterImage.classList.add('gold-border');
-            } else if (category === 'Pilgrim') {
-                characterImage.classList.add('orange-border');
-            } else if (category === 'SR') {
-                characterImage.classList.add('purple-border');
-            } else if (category === 'R') {
-                characterImage.classList.add('blue-border');
-            }
-        }
+      characterImage.src = character.image;
+      characterImage.alt = character.name;
+      characterImage.className = "gacha-card"; 
+
+      // Determine rarity from category
+      var category = Object.keys(charactersData).find(key => charactersData[key].includes(character));
+      switch(category) {
+        case 'SSR':
+          characterImage.classList.add('gold-border');
+          break;
+        case 'Pilgrim':
+          characterImage.classList.add('orange-border');
+          break;
+        case 'SR':
+          characterImage.classList.add('purple-border');
+          break;
+        case 'R':
+          characterImage.classList.add('blue-border');
+          break;
+      }
         gachaWindow.appendChild(characterImage);
-    } catch (error) {
-        console.error('Error displaying character:', error);
+      } catch (error) {
+          console.error('Error displaying character:', error);
     }
   }
 
@@ -117,7 +134,6 @@ fetch('./data/characters.json')
     init(data);
   })
   .catch(error => {
-    // catch error
     console.error('Error loading characters data:', error);
 });
 
